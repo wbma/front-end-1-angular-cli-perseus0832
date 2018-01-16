6388c255f6d796382adc30d61d3dcfff9610bf2e
perseus0832 <nickvo2308@gmail.com>
2018-01-16 16:25:50 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainContentComponent,
+    SidebarComponent,
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
